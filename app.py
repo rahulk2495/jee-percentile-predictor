fde24426a2fc7df5b1173697b9344b3b0b6f9dc7
rahulk2495 <97147614+rahulk2495@users.noreply.github.com>
2025-08-22 19:55:28 +0530

    Update app.py

--- app.py
+++ app.py
@@ -113,7 +113,7 @@ use_sample = st.checkbox("Use sample data instead of uploading Excel?")
 
 if use_sample:
     st.write("Edit the sample data below and predictions will update automatically:")
-    st.write("When you play wround with the data, make sure you choose realistic values for 10th Math and 10th Science score as to students are expected to above 70 to get PCM in 11th:")
+    st.write("When you play around with the data, make sure you choose realistic values for 10th Math and 10th Science score as students are expected score above 70 in 10th Math and Science to get PCM in 11th in JNVs:")
      
     df = st.data_editor(df_sample, num_rows="dynamic")
     
@@ -230,6 +230,7 @@ Predicted Percentile = 196.20  - 6.34 × Female  - 2.53 × Math + 0.0202 × Math
         
     
     
+
 
 
 
